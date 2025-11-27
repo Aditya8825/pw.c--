5b66c6ba8c07b2859335d2ba08b55ceaf8d63343
Aditya <aditya998887766@gmail.com>
2025-11-27 23:58:39 +0530

    first commit

---
+++ push back ll.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace  std;
+
+
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List {
+    Node* head;
+    Node* tail;
+
+public:
+    List() {
+        head = tail = NULL;
+    }
+
+
+
+    void push_front(int val) {
+        Node* newNode = new Node(val);
+
+        if (head == NULL) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+     void push_back(int val){
+        Node* newNode = new Node(val);
+         
+        if(head==NULL){
+            head=tail=newNode;
+        }else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+     }
+
+    void printlinklist() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout <<"null" <<endl;
+    }
+};
+
+int main() {
+    List linklist;
+
+    linklist.push_front(1);
+    linklist.push_front(2);
+    linklist.push_front(3);
+     linklist.push_front(4);
+    linklist.printlinklist();
+
+    return 0;
+}
+
+ 
